4dc9736f33f913570e3fa2525725aeb586b74a38
Billy Huang <10646003@ntub.edu.tw>
2019-09-27 16:51:18 +0800

    components - init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Lesson1Component } from './components/lesson1/lesson1.component';
+import { Lesson2Component } from './components/lesson2/lesson2.component';
+import { Lesson3Component } from './components/lesson3/lesson3.component';
+import { Homework1Component } from './components/homework1/homework1.component';
+import { Lesson4Component } from './components/lesson4/lesson4.component';
+import { Lesson5Component } from './components/lesson5/lesson5.component';
+import { Lesson7Component } from './components/lesson7/lesson7.component';
+import { Lesson8Component } from './components/lesson8/lesson8.component';
+import { ModelDrivenComponent } from './components/model-driven/model-driven.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Lesson1Component,
+    Lesson2Component,
+    Lesson3Component,
+    Homework1Component,
+    Lesson4Component,
+    Lesson5Component,
+    Lesson7Component,
+    Lesson8Component,
+    ModelDrivenComponent
   ],
   imports: [
     BrowserModule,
